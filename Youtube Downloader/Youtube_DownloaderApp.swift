19a921937381ff3edce53ba4ab2a85505753bb32
Sharad Jadhav <jadhavsharad290@gmail.com>
2025-07-19 03:14:37 +0530

    Initial Commit

---
+++ Youtube Downloader/Youtube_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_DownloaderApp.swift
+//  Youtube Downloader
+//
+//  Created by Sharad Jadhav on 19/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
